56228fd6c6b4132057d7ec5a730127fda1cabd17
ikinoti <gitongakinoti1@gmail.com>
2024-05-16 02:51:43 +0300

    modified for rest framework

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'watchlist_app',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
