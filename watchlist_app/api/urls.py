cc309f58a97a5b54c2a616ab309f8da558985009
ikinoti <gitongakinoti1@gmail.com>
2024-05-17 03:13:45 +0300

    modified our path endpoints to use class based view

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import movie_list, movie_details
+# from .views import movie_list, movie_details
+from .views import MovieListAV, MovieDetailAV
 
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_details, name='movie_details')
+    # path('list/', movie_list, name='movie-list'),
+    # path('<int:pk>', movie_details, name='movie_details'),
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>', MovieDetailAV.as_view(), name='movie_details')
 ]
