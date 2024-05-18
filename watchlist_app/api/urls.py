19f3c6c9e89caa20426cc1f313a6a1a16622ab62
ikinoti <gitongakinoti1@gmail.com>
2024-05-18 03:59:54 +0300

    modified to match our changed views

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 # from .views import movie_list, movie_details
-from .views import MovieListAV, MovieDetailAV
+from .views import WatchListAV, WatchDetailAV, StreamPlatformAV
 
 
 urlpatterns = [
     # path('list/', movie_list, name='movie-list'),
     # path('<int:pk>', movie_details, name='movie_details'),
-    path('list/', MovieListAV.as_view(), name='movie-list'),
-    path('<int:pk>', MovieDetailAV.as_view(), name='movie_details')
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>', WatchDetailAV.as_view(), name='movie_details'),
+    path('stream/', StreamPlatformAV.as_view(), name='stream')
 ]
