5ca59ad3e89ad0c0f338aa26bff6558ab42d4389
ikinoti <gitongakinoti1@gmail.com>
2024-05-21 01:22:35 +0300

    endpoint for stream functionality

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 # from .views import movie_list, movie_details
-from .views import WatchListAV, WatchDetailAV, StreamPlatformAV
+from .views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
 
 
 urlpatterns = [
     # path('list/', movie_list, name='movie-list'),
     # path('<int:pk>', movie_details, name='movie_details'),
-    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('list/', WatchListAV.as_view(), name='movie_list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie_details'),
-    path('stream/', StreamPlatformAV.as_view(), name='stream')
+    path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream_detail')
 ]
