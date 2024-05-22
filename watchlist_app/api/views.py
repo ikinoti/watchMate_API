33f953b3d3ec3855c95c8988a93d373cab5b08d4
ikinoti <gitongakinoti1@gmail.com>
2024-05-22 03:51:08 +0300

    model view set

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -56,19 +56,35 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 
 #  $$$$ END OF MIXIN GENERIC API VIEW $$$$$$
 
-# VIEW SETS VIEW
-class StreamPlatformVS(viewsets.ViewSet):
+# model views set
+class StreamPlatformVS(viewsets.ModelViewSet):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
 
-    def list(self, request):
-        queryset = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(queryset, many=True)
-        return Response(serializer.data)
+# end of model view set
+
+# VIEW SETS VIEW
+# class StreamPlatformVS(viewsets.ViewSet):
+
+#     def list(self, request):
+#         queryset = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(queryset, many=True)
+#         return Response(serializer.data)
     
-    def retrieve(self, request, pk=None):
-        queryset = StreamPlatform.objects.all()
-        watchlist = get_object_or_404(queryset, pk=pk)
-        serializer = StreamPlatformSerializer(watchlist)
-        return Response(serializer.data)
+#     def retrieve(self, request, pk=None):
+#         queryset = StreamPlatform.objects.all()
+#         watchlist = get_object_or_404(queryset, pk=pk)
+#         serializer = StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
+    
+#     def create(self, request):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+        
 
 # END OF VIEW SETS VIEW
 class StreamPlatformAV(APIView):
