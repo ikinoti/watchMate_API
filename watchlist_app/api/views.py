1c09245f5a829559c971011151bca14288b2d8ea
ikinoti <gitongakinoti1@gmail.com>
2024-05-23 00:45:05 +0300

    enabled reviews to be ttached to a user

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.exceptions import ValidationError
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import status
@@ -16,11 +17,20 @@ from .serializers import (WatchListSerializer,
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
 
-        serializer.save(watchlist = watchlist)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already revieweed this watch!")
+
+        serializer.save(watchlist = watchlist, review_user=review_user)
         
 
 class ReviewList(generics.ListAPIView):
