235d98fb7794e9fd60b47c89eca70239701bec70
ikinoti <gitongakinoti1@gmail.com>
2024-05-28 01:56:09 +0300

    logout view

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -6,7 +6,12 @@ from rest_framework import status
 from user_app import models
 
 
+@api_view(['POST',])
+def logout_view(request):
 
+    if  request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['POST',])
