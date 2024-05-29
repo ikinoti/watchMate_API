94def92191dd0220760f6a7a2df2e66aeaff86d9
ikinoti <gitongakinoti1@gmail.com>
2024-05-29 03:58:22 +0300

    jwt

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,6 +3,7 @@ from .serializers import RegistrationSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+# from rest_framework_simplejwt.tokens import RefreshToken
 from user_app import models
 
 
@@ -31,6 +32,12 @@ def registration_view(request):
 
             token = Token.objects.get(user=account).key
             data['token'] = token
+
+            # refresh = RefreshToken.for_user(account)
+            # data['token'] = {
+            #     'refresh': str(refresh),
+            #     'access': str(refresh.access_token),
+            # }
         
         else:
             data = serializer.errors
