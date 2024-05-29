6f524bac20790e6be62d0eea4c29af83d519b530
ikinoti <gitongakinoti1@gmail.com>
2024-05-29 03:55:55 +0300

    registering throttle functionality

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -135,11 +135,22 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
 
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.BasicAuthentication',
-    # ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle'
+    # ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/day',
+        'user' : '5/day'
+    }
 }
+
+# SIMPLE_JWT = {
+#     'ROTATE_REFRESH_TOKENS' : True
+# }
