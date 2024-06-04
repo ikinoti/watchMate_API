680fe32549328082446ec70ae412a756cf6ff147
ikinoti <gitongakinoti1@gmail.com>
2024-06-05 02:55:07 +0300

    end point for filtering and searching

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 # from .views import movie_list, movie_details
 from .views import (WatchListAV, WatchDetailAV,
                     StreamPlatformAV,StreamPlatformDetailAV, StreamPlatformVS, ReviewList,
-                    ReviewDetail, ReviewCreate)
+                    ReviewDetail, ReviewCreate, UserReview)
 
 router = DefaultRouter()
 router.register('stream', StreamPlatformVS, basename='streamplatform')
@@ -26,4 +26,6 @@ urlpatterns = [
     path('<int:pk>/review-create/', ReviewCreate.as_view(), name='review_create'),
     path('<int:pk>/reviews/', ReviewList.as_view(), name='review_list'),
     path('review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
+
+    path('reviews/', UserReview.as_view(), name='user-review-detail')
 ]
