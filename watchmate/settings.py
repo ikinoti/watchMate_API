3e03ec80d0a2b1e3984ff2acd2d91e3bf3ea0dab
ikinoti <gitongakinoti1@gmail.com>
2024-06-04 23:31:01 +0300

    throttle setting

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -147,7 +147,10 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '2/day',
-        'user' : '5/day'
+        'user' : '5/day',
+        'review-create' : '1/day',
+        'review-list' : '10/day',
+        'review-detail': '2/day'
     }
 }
 
