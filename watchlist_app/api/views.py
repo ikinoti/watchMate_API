285df34849898d502036698bd8560a8ec758018a
ikinoti <gitongakinoti1@gmail.com>
2024-06-11 02:22:14 +0300

    code cleaning

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,14 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.exceptions import ValidationError
-from rest_framework import mixins
-from rest_framework import generics
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import generics, status, viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle, ScopedRateThrottle
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
